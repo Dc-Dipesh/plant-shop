ae54ddaf09d160e1f4dce443f0889cac134232ea
Dipesh Chaulagain <dcdipesh1998@gmail.com>
2024-02-28 20:48:31 +0545

    profile shop and plant details page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import Sidebar from "@/components/Sidebar"
-import { ThemeProvider } from "next-themes"
 
 const inter = Inter({ subsets: ["latin"] })
 
